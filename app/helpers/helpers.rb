d80548bd783adb4a20cc6b0afcf02deb2c928ff5
Dakota Clark <dakotaclark92@yahoo.com>
2019-09-04 01:36:47 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,12 +1,12 @@
 class Helpers
   
-def
-  current_user
-end
 
-def
-  is_logged_in?
-end
+  def self.current_user(session)
+    User.find(session[:user_id])
+  end
 
-  
+  def self.is_logged_in?(session)
+    !!session[:user_id]
+  end
+
 end
